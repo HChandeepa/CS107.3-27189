6f94fc313fb2ac380aefb381872e15312c54e005
HChandeepa <118461284+HChandeepa@users.noreply.github.com>
2023-06-22 14:48:14 +0530

    2023.06.21 questions

---
+++ Lab-2023.6.21/Q1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2023._06._21Q1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Add a separate class file to Console application program and create a method call private void sayHello().
+
+Inside the method display hello world
+
+main class create a class object and try to access the sayHello() method by using the class
+
+Can you access the method? Explain why?*/
+
+            hello objhello = new hello();
+            objhello.sayHello();
+
+            Console.ReadKey();
+
+            /*Program is not working because of the class is private*/
+        }
+    }
+}
---
+++ Lab-2023.6.21/Q1/hello.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2023._06._21Q1
+{
+    internal class hello
+    {
+        private void sayHello()
+        {
+            Console.WriteLine("Hello world!");
+        }
+    }
+}
