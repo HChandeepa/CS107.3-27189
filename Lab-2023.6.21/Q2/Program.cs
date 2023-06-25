aae44f346e6c6207e057ec4451f543e738b258a8
HChandeepa <118461284+HChandeepa@users.noreply.github.com>
2023-06-25 14:42:36 +0530

    2023.06.21 Q2

---
+++ Lab-2023.6.21/Q2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2023._06._21Q2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* declare a single dimensional array with 10 elements .Input the values to the array and find the followings
+             1)minimum value
+            2)maximum value
+            3)average value
+            4)reverse order of values
+            hint-use a method which in seperate class .and call the method from main the method*/
+
+
+            array objarray = new array();
+            objarray.myarray();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab-2023.6.21/Q2/array.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2023._06._21Q2
+{
+    internal class array
+    {
+        public void myarray()
+        {
+
+            int[] narray = new int[10];
+            float sum = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("Enter value {0}: ", i);
+                narray[i] = int.Parse(Console.ReadLine());
+                sum += narray[i];
+            }
+            float avg = sum / 10;
+
+            int max = narray[0];
+            int min = narray[0];
+
+            for (int i = 1; i < 10; i++)
+            {
+                if (narray[i] > max)
+                    max = narray[i];
+
+                if (narray[i] < min)
+                    min = narray[i];
+            }
+
+            Console.WriteLine("Minimum value: " + min);
+            Console.WriteLine("Maximum value: " + max);
+            Console.WriteLine("Average value:" + avg);
+
+            for(int i=9;i>=0;i--)
+            {
+                Console.Write("{0} ", narray[i]);
+            }
+        }
+    }
+}
