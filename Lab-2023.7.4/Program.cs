c1ba7d3f88499cb798b762f9bc3b0077f892ffec
HChandeepa <118461284+HChandeepa@users.noreply.github.com>
2023-07-04 10:38:25 +0530

    lab 2023.7.4

---
+++ Lab-2023.7.4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2023._06._23Q1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Question 06 Take time to think and complete the question...
+
+Create a Console application with two classes (Main class + another class).
+Inside the main class take a user input which is the size of the array.
+Pass the user inserted size as a parameter to the added class method. Inside the method create an integer array based on passed value from 
+main method.Then take user inputs for the created array inside the separate class.Every user input value should be followed by value 0 inside 
+the array.
+Eg Assume array size is 9 and it should as follows*   {u input 0 u input 0  u input 0 u input}*/
+
+            Console.WriteLine("Enter size of the array:");
+            int no = int.Parse(Console.ReadLine());
+
+            array objarray = new array();
+            objarray.size(no);
+
+            
+
+            
+
+            Console.ReadKey();
+           
+            
+
+
+        }
+    }
+}
---
+++ Lab-2023.7.4/array.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2023._06._23Q1
+{
+    internal class array
+    {
+        public void size(int no)
+        {
+            int[] arr = new int[no];
+           
+            for (int i = 0; i <arr.Length; i++)
+            {
+                if(i%2==1)
+                {
+                    arr[i] = 0;
+                }
+                if(i%2==0)
+                {
+                    Console.WriteLine("Enter {0}:", arr[i]);
+                    arr[i] = int.Parse(Console.ReadLine());
+                }
+            }
+            Console.WriteLine("\nDisplay array:");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+
+            }
+        }
+    }
+}
